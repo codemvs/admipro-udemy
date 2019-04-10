02cd2ec50bd80064a28d30582f32dd73bfe4e399
Git MCS <mv.salvador93@gmail.com>
2019-04-09 22:05:59 -0500

    Creando usuarios FE y validaciones p1

--- src/app/login/register.component.ts
+++ src/app/login/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+declare function init_plugings();
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class RegisterComponent implements OnInit {
+  
+  forma: FormGroup;
 
   constructor() { }
 
   ngOnInit() {
+    init_plugings();
+  // Forms validator
+    this.forma = new FormGroup({
+      nombre: new FormControl(null, Validators.required),
+      correo: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, Validators.required),
+      password2: new FormControl(null, Validators.required),
+      condiciones: new FormControl( false )
+    });
+
+  }
+
+  registarUsuario() {
+    console.log( this.forma.value );
+    
   }
 
 }
