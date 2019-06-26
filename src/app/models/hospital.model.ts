08246ba49176719492fe5947f33d50483cd0d4e3
codemvs <codemvs@gmail.com>
2019-06-26 17:30:09 -0500

    Avances tarea 16

---
+++ src/app/models/hospital.model.ts
@@ -0,0 +1,6 @@
+export class Hospital {
+    usuario: string;
+    nombre: string;    
+    img: string;
+    _id: string;
+}
--- src/app/services/hospital/hospital.service.ts
+++ src/app/services/hospital/hospital.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Hospital } from 'src/app/models/hospital.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,16 @@ import { Injectable } from '@angular/core';
 export class HospitalService {
 
   constructor() { }
+
+  cargarHospitales() {}
+
+  obtenerHospital( id: string ) {}
+
+  borrarHospital( id: string ) {}
+
+  crearHospital( nombre: string ) {}
+
+  buscarHospital( termino: string ) {}
+
+  actualizarHospital( hospital: Hospital ) {}
 }
