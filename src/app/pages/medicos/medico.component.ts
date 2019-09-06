f683124044d416aefba36828c2a11860fc335970
codemvs <codemvs@gmail.com>
2019-09-05 22:14:46 -0500

    Medicos v2

--- src/app/pages/medicos/medico.component.ts
+++ src/app/pages/medicos/medico.component.ts
@@ -3,6 +3,7 @@ import { NgForm } from '@angular/forms';
 import { Hospital } from 'src/app/models/hospital.model';
 import { MedicoService, HospitalService } from 'src/app/services/services.index';
 import { Medico } from 'src/app/models/medico.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-medico',
@@ -10,26 +11,45 @@ import { Medico } from 'src/app/models/medico.model';
   styles: []
 })
 export class MedicoComponent implements OnInit {
+
   hospitales: Hospital[] = [];
-  medico: Medico = new Medico();
+  medico: Medico = new Medico('', '', '', '', '');
+  hospital: Hospital = new Hospital('', '', null);
+
   constructor(
     public _medicoService: MedicoService,
-    public _hospitalService: HospitalService
+    public _hospitalService: HospitalService,
+    public router: Router
   ) { }
 
   ngOnInit() {
     this._hospitalService.cargarHospitales().subscribe((hospitales: any) => {
       this.hospitales = hospitales.hospitales;
       console.log(hospitales);
-      
+
     });
-    
-    
+
+
   }
   guardarMedico(f: NgForm) {
-    console.log(f.valid);
-    console.log(f.value);
 
+    if (f.invalid) {
+      return false;
+    }
+    this._medicoService.guardarMedico(this.medico)
+      .subscribe((medico: any) => {
+        this.medico._id = medico._id;
+        this.router.navigate(['/medico', medico._id]);
+      });
+  }
 
+  cambioHospital(id: string) {
+    console.log(id);
+
+    this._hospitalService.obtenerHospital(id).subscribe((hospital: any) => {
+      this.hospital = hospital;
+      console.log(this.hospital);
+
+    });
   }
 }
--- src/app/services/medico/medico.service.ts
+++ src/app/services/medico/medico.service.ts
@@ -4,6 +4,7 @@ import { URL_SERVICIOS } from 'src/app/config/config';
 import { map } from 'rxjs/operators';
 import { UsuarioService } from '../usuario/usuario.service';
 import Swal from 'sweetalert2';
+import { Medico } from 'src/app/models/medico.model';
 
 @Injectable({
   providedIn: 'root'
@@ -40,6 +41,15 @@ export class MedicoService {
         return resp;
       })
     );
-
+  }
+  guardarMedico(medico: Medico) {
+    let url = `${URL_SERVICIOS}/medico?token=${this._usuarioService.token}`;
+    return this.http.post(url, medico)
+      .pipe(
+        map((resp: any) => {
+          Swal.fire('Medico creado', medico.nombre, 'success');
+          return resp.medico;
+        })
+      );
   }
 }
