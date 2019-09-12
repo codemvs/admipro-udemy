3f2ec06a13187db82dc9a56bf960605f5ec07318
codemvs <codemvs@gmail.com>
2019-09-11 22:10:01 -0500

    Busqeuda todo listo

--- src/app/pages/busqueda/busqueda.component.ts
+++ src/app/pages/busqueda/busqueda.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from 'src/app/config/config';
+import { Usuario } from 'src/app/models/usuario.model';
+import { Medico } from 'src/app/models/medico.model';
+import { Hospital } from 'src/app/models/hospital.model';
+
 
 @Component({
   selector: 'app-busqueda',
@@ -8,18 +14,35 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class BusquedaComponent implements OnInit {
 
-  constructor(  public activatedRoute: ActivatedRoute) {
+  usuarios: Usuario[] = [];
+  medicos: Medico[] = [];
+  hospitales: Hospital[] = [];
+
+  constructor(public activatedRoute: ActivatedRoute,
+    public http: HttpClient
+  ) {
 
     activatedRoute.params.subscribe(
       (params: any) => {
         let termino = params.termino;
-        console.log(termino);
-        
+        this.buscar(termino);
       }
     );
-   }
+  }
 
   ngOnInit() {
+  }
+
+  buscar(termino: string) {
+    let url = `${URL_SERVICIOS}/busqueda/todo/${termino}`;
+    this.http.get(url).subscribe((resp: any) => {
+      this.usuarios = resp.usuarios;
+      this.medicos = resp.medicos;
+      this.hospitales = resp.hospitales;
+
+      console.log(resp);
+      
+    });
   }
 
 }
